71a42e5ff348c40590d8c1a913ed6ffb0e0592f1
Bepoz <45073750+Be-poz@users.noreply.github.com>
2021-09-06 01:04:36 +0900

    refactor: test 추가

--- app/src/test/java/nextstep/learning/http/ThreadTest.java
+++ app/src/test/java/nextstep/learning/http/ThreadTest.java
@@ -2,13 +2,72 @@ package nextstep.learning.http;
 
 import org.junit.jupiter.api.Test;
 
-import java.util.concurrent.CountDownLatch;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
+import java.util.concurrent.*;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.setMaxElementsForPrinting;
 
 class ThreadTest {
+
+    @Test
+    void countDownLatchTest() throws InterruptedException {
+        int numberOfThreads = 3;
+        CountDownLatch latch = new CountDownLatch(3);
+        ExecutorService service = Executors.newFixedThreadPool(numberOfThreads);
+        MyCounter counter = new MyCounter();
+        System.out.println(latch.getCount());
+        latch.countDown();
+        System.out.println(latch.getCount());
+        latch.countDown();
+        System.out.println(latch.getCount());
+
+//        for (int i = 0; i < numberOfThreads; i++) {
+//            service.execute(() -> {
+//                counter.increment();
+//                latch.countDown();
+//            });
+//        }
+//        latch.await();
+    }
+
+    @Test
+    void semaphoreTest() throws InterruptedException {
+        Semaphore semaphore = new Semaphore(10);
+        ExecutorService service = Executors.newFixedThreadPool(10);
+        for (int i = 0; i < 10; i++) {
+            semaphore.acquire();
+        }
+        assertThat(semaphore.tryAcquire()).isFalse();
+        assertThat(semaphore.tryAcquire(0)).isTrue();
+        assertThat(semaphore.tryAcquire(5)).isFalse();
+        semaphore.release(10);
+        assertThat(semaphore.tryAcquire(10)).isTrue();
+    }
+
+    @Test
+    void cyclicBarrierTest() throws InterruptedException, BrokenBarrierException {
+        CyclicBarrier barrier = new CyclicBarrier(5);
+        ExecutorService service = Executors.newFixedThreadPool(5);
+        for (int i = 0; i < 5 ; i++) {
+            service.execute(() -> {
+                try {
+                    barrier.await();
+                    System.out.println("bepoz");
+                } catch (InterruptedException | BrokenBarrierException e) {
+                    e.printStackTrace();
+                }
+            });
+        }
+        Thread.sleep(100);
+        System.out.println("a");
+        barrier.await();
+        barrier.await();
+        barrier.await();
+        barrier.await();
+        barrier.await();
+        System.out.println("b");
+    }
+
 
     @Test
     void testCounterWithConcurrency() throws InterruptedException {
